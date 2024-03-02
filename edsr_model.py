6c2e3961da95e01f7eb9f653ab81c777e1c81c89
Hardik Raina <hardikraina079@gmail.com>
2024-03-02 20:34:47 +0530

    Overhauled logic for CUDA use

--- edsr_model.py
+++ edsr_model.py
@@ -1,60 +1,150 @@
 import torch
 import torch.nn as nn
 
-kernel_size = 3
-padding=1
-
-class ResBlock(nn.Module):
-    def __init__(self, channels = 256):
-        super(ResBlock, self).__init__()
-
-        self.conv = nn.Conv2d( in_channels = channels, 
-                               out_channels = channels, 
-                               kernel_size = kernel_size, 
-                               padding = padding)
+class ConvBlock(nn.Module):
+    def __init__(self, 
+                 in_channels, 
+                 out_channels, 
+                 discriminator = False, 
+                 use_act = True, 
+                 use_bn = True, 
+                 **kwargs):
         
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        identity = x                # Trunk/Main branch
-        x = self.conv(x)
-        x = self.relu(x)
-        x = self.conv(x)
-        x *= 0.1                    # Scaling factor
-        x = torch.add(x, identity)  # Merge branch back
-        return x
-
-class EDSR(nn.Module):
-    def __init__(self, channels = 256, num_blocks = 32):
-        super(EDSR, self).__init__()
-
-        col = 3
-
-        self.conv1 = nn.Conv2d(in_channels = col,               # Input Conv
-                               out_channels = channels, 
-                               kernel_size = kernel_size, 
-                               padding = 1)
+        super().__init__()
+        self.use_act = use_act
+        self.bn = nn.BatchNorm2d(out_channels) if use_bn else nn.Identity()
+        self.cnn = nn.Conv2d(in_channels = in_channels, 
+                             out_channels = out_channels, 
+                             **kwargs, 
+                             bias = not use_bn)
         
-        self.conv_out = nn.Conv2d(in_channels = channels, 
-                                  out_channels = col, 
-                                  kernel_size = kernel_size, 
-                                  padding = 1)
-        
-        self.conv_up = nn.Conv2d(in_channels = channels,        # For upsampling
-                                 out_channels = channels * 4, 
-                                 kernel_size = kernel_size, 
-                                 padding = 1)
-
-        # A range of ResBlocks in a sequential format
-        self.body = nn.Sequential(*(ResBlock() for _ in range(num_blocks)))
-
-        self.upsample = nn.Sequential(self.conv_up, nn.PixelShuffle(2))
-
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.body(out)
-        out = self.upsample(out)
-        out = self.conv_out(out)
-
-        return out
+        self.act = (nn.LeakyReLU(negative_slope = 0.2, 
+                                 inplace=True) if discriminator else nn.PReLU(num_parameters = out_channels))
     
+    def forward(self, x):
+        return self.act(self.bn(self.cnn(x))) if self.use_act else self.bn(self.cnn(x))
+    
+class UpSampleBlock(nn.Module):
+    def __init__(self, 
+                 in_c, 
+                 scale_factor):
+        
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels = in_c, 
+                              out_channels = in_c * scale_factor**2, 
+                              kernel_size = 3, 
+                              stride = 1, 
+                              padding = 1)
+        self.ps = nn.PixelShuffle(scale_factor)
+        self.act = nn.PReLU(num_parameters = in_c)
+    
+    def forward(self, x):
+        return self.act(self.ps(self.conv(x)))
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.block1 = ConvBlock(in_channels = in_channels, 
+                                out_channels = in_channels, 
+                                kernel_size = 3, 
+                                stride = 1, 
+                                padding = 1)
+        
+        self.block2 = ConvBlock(in_channels = in_channels, 
+                                out_channels = in_channels, 
+                                kernel_size = 3, 
+                                stride = 1, 
+                                padding = 1,
+                                use_act = False)
+    
+    def forward(self, x):
+        out = self.block1(x)
+        out = self.block2(out)
+        return out + x
+
+class Generator(nn.Module):
+    def __init__(self, 
+                 in_channels = 3, 
+                 num_channels = 64, 
+                 num_blocks = 16):
+        
+        super().__init__()
+        self.initial = ConvBlock(in_channels = in_channels, 
+                                 out_channels = num_channels, 
+                                 kernel_size = 9, 
+                                 stride = 1, 
+                                 padding = 4, 
+                                 use_bn=False)
+        
+        self.residuals = nn.Sequential(*[ResidualBlock(num_channels) for _ in range(num_blocks)])
+        
+        self.convblock = ConvBlock(in_channels = num_channels, 
+                                   out_channels =num_channels, 
+                                   kernel_size = 3, 
+                                   stride = 1, 
+                                   padding = 1, 
+                                   use_act=False)
+        
+        self.upsamples = nn.Sequential(UpSampleBlock(num_channels, 2), 
+                                       UpSampleBlock(num_channels, 2))
+        
+        self.final = nn.Conv2d(in_channels = num_channels, 
+                               out_channels =in_channels, 
+                               kernel_size = 9, 
+                               stride = 1, 
+                               padding = 4)
+    
+    def forward(self, x):
+        initial = self.initial(x)
+        x = self.residuals(initial)
+        x = self.convblock(x) + initial
+        x = self.upsamples(x)
+        return torch.tanh(self.final(x))
+
+class Discriminator(nn.Module):
+    def __init__(self, 
+                 in_channels = 3, 
+                 features = [64, 64, 128, 128, 256, 256, 512, 512]):
+        
+        super().__init__()
+
+        blocks = []
+
+        for idx, feature in enumerate(features):
+            blocks.append(ConvBlock(in_channels = in_channels, 
+                                    out_channels = feature, 
+                                    kernel_size = 3, 
+                                    padding = 1,
+                                    stride = 1 + idx % 2, 
+                                    discriminator = True, 
+                                    use_act = True,
+                                    use_bn = False if idx == 0 else True))
+            in_channels = feature
+
+        self.block = nn.Sequential(*blocks)
+
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((8, 8)),
+            nn.Flatten(),
+            nn.Linear(512 * 8 * 8, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, 1)
+        )
+    
+    def forward(self, x):
+        x = self.block(x)
+        return self.classifier(x)
+
+def edsr_test():
+    low_resolution = 24
+    with torch.gpu.amp.autocast():
+        x = torch.randn((5, 3, low_resolution, low_resolution))
+        gen = Generator()
+        gen_out = gen(x)
+        disc = Discriminator()
+        disc_out = disc(gen_out)
+
+        print(gen_out.shape)
+        print(disc_out.shape)
+        print(list(gen.parameters()))
+        print(list(disc.parameters()))
