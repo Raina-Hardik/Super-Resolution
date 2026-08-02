547385eb68352158fd7f2dc0aaf3877c6ca6a42d
Hardik Raina <hardikraina079@gmail.com>
2026-08-03 00:58:44 +0530

    fix: update loss module and dataset loader imports

--- models/loss.py
+++ models/loss.py
@@ -1,17 +1,17 @@
 import torch.nn as nn
-from torchvision.models import vgg19
-import config
+from torchvision.models import vgg19, VGG19_Weights
+from core.config import settings
 
 class VGGLoss(nn.Module):
     def __init__(self):
         super().__init__()
-        self.vgg = vgg19(pretrained=True).features[:36].eval().to(config.DEVICE)
+        self.vgg = vgg19(weights=VGG19_Weights.DEFAULT).features[:36].eval().to(settings.device)
         self.loss = nn.MSELoss()
 
         for params in self.vgg.parameters():
             params.requires_grad = False
     
-    def forward(self,input,target):
+    def forward(self, input, target):
         vgg_input_features = self.vgg(input)
         vgg_target_features = self.vgg(target)
-        return self.loss(vgg_input_features,vgg_target_features)
+        return self.loss(vgg_input_features, vgg_target_features)
